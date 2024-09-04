6bb7c6ab2f7db26814511350805e0af93852aba3
jaemin <jjm7655@gmail.com>
2024-09-04 20:47:38 +0900

    #2:댓글 관리 서비스의 비즈니스 로직 및 컨트롤러 테스트 작성

--- src/main/java/com/fastcampus/projectboardadmin/dto/properties/ProjectProperties.java
+++ src/main/java/com/fastcampus/projectboardadmin/dto/properties/ProjectProperties.java
@@ -10,10 +10,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("project")
 public record ProjectProperties(Board board) {
 
-    /**
-     * 게시판 관련 프로퍼티
-     *
-     * @param url 게시판 서비스 호스트명
-     */
-    public record Board(String url) {}
+	/**
+	 * 게시판 관련 프로퍼티
+	 *
+	 * @param url 게시판 서비스 호스트명
+	 */
+	public record Board(String url) {}
 }
